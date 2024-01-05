60a3d0884db4202105e256994cf30919442f2d68
Zeshanulhaq <zeshanulhaq1655@gmail.com>
2024-01-05 20:05:48 +0500

    initial-commit

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import "./globals.css";
+import Navbar from "@/containers/Navbar/Navbar";
+import { Providers } from "./providers";
+
+export const metadata = {
+  title: "Portfolio | Ahtasham",
+  description: "My Portfolio website.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="app dark:bg-dark-bg overflow-x-hidden">
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
